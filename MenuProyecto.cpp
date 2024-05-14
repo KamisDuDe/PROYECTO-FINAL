95ce033d44a4a12378a9bd31c61841dd8f18dc7d
Eduardo Portillo <157424570+KamisDuDe@users.noreply.github.com>
2024-05-13 18:07:21 -0600

    Add files via upload

--- MenuProyecto.cpp
+++ MenuProyecto.cpp
@@ -21,9 +21,11 @@ int main()
                << endl;
           cout << "\t[4] POO.\n"
                << endl;
-          cout << "\t[5] Herencia y Polimorfismo..\n"
+          cout << "\t[5] Herencia y Polimorfismo.\n"
                << endl;
           cout << "\t[6] Base de datos.\n"
+               << endl;
+          cout << "\t[7] Salir del programa.\n"
                << endl;
 
           cout << "\t  ┌──────────────────────┐" << endl;
@@ -44,16 +46,21 @@ int main()
 
           case 4:
                break;
+
           case 5:
                break;
+
           case 6:
+               break;
+
+          case 7:
                break;
 
           default:
                cout << "Opcion incorrecta." << endl;
                break;
           }
-     } while (Imp != 7);
+     } while (Imp != 8);
 
      system("pause");
      return 0;
