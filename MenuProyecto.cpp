c2c6555702acbd5a1d27048773a1f275f7a53d3d
Eduardo Portillo <157424570+KamisDuDe@users.noreply.github.com>
2024-05-13 18:00:57 -0600

    Add files via upload

---
+++ MenuProyecto.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+     int Imp;
+
+     do
+     {
+          cout << "\t\t╔═══════════════════╗" << endl;
+          cout << "\t\t║  Proyecto Final   ║" << endl;
+          cout << "\t\t╚═══════════════════╝" << endl;
+
+          cout << "\t[1] Condicionales y Ciclos.\n"
+               << endl;
+          cout << "\t[2] Funciones.\n"
+               << endl;
+          cout << "\t[3] Archivos.\n"
+               << endl;
+          cout << "\t[4] POO.\n"
+               << endl;
+          cout << "\t[5] Herencia y Polimorfismo..\n"
+               << endl;
+          cout << "\t[6] Base de datos.\n"
+               << endl;
+
+          cout << "\t  ┌──────────────────────┐" << endl;
+          cout << "\t  ╞ Elije una funcion:   ╡" << endl;
+          cout << "\t  └──────────────────────┘";
+          cin >> Imp;
+
+          switch (Imp)
+          {
+          case 1:
+               break;
+
+          case 2:
+               break;
+
+          case 3:
+               break;
+
+          case 4:
+               break;
+          case 5:
+               break;
+          case 6:
+               break;
+
+          default:
+               cout << "Opcion incorrecta." << endl;
+               break;
+          }
+     } while (Imp != 7);
+
+     system("pause");
+     return 0;
+}
